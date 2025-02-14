c377659cb80eb779d5efe6cf5ff2f74d85b52eaf
GOPUBHAVANI <nigabhavi@132gmail.com>
2025-02-14 19:47:34 +0530

    To-Do-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Todoapp from './pages/Todoapp'
 
 const App = () => {
   return (
-    <div>
-      <p>React dev</p>
-    </div>
+    <>
+    <Todoapp/>
+    </>
   )
 }
 
